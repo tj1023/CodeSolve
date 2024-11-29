68d9e48ea9fb4e6d979ddd886df58fd2bbd77202
tj1023 <htjeong1023@naver.com>
2024-11-29 18:33:04 +0900

    BOJ_13459, BOJ_13460, BOJ_15644, BOJ_15653 #구슬탈출 #BFS #구현

---
+++ BOJ/BOJ_15644.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef pair<int, int> point;
+
+int n, m;
+
+struct node {
+	int y, x, depth;
+	string dir;
+};
+
+pair<int, string> bfs(point hole, point red, point blue, vector<vector<char>> board) {
+	queue<node> rq;
+	queue<point> bq;
+	bool visited[10][10][10][10] = {};
+	rq.push({ red.first, red.second, 0, "" });
+	bq.push(blue);
+	visited[red.first][red.second][blue.first][blue.second] = true;
+
+	while (!rq.empty() && !bq.empty()) {
+		node curr = rq.front(); rq.pop();
+		point curb = bq.front(); bq.pop();
+
+		// ╩С
+		point nextr = { curr.y, curr.x };
+		point nextb = curb;
+		bool canMove = true;
+		bool redGoal = false;
+		if (curr.y < curb.first) {
+			while (nextr.first - 1 >= 0 && board[nextr.first - 1][nextr.second] != '#') {
+				nextr.first--;
+				if (board[nextr.first][nextr.second] == 'O') {
+					redGoal = true;
+					nextr = { 0,0 };
+					break;
+				}
+			}
+			while (nextb.first - 1 >= 0 && board[nextb.first - 1][nextb.second] != '#' &&
+				!(nextr.first == nextb.first - 1 && nextr.second == nextb.second)) {
+				nextb.first--;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (redGoal && canMove) return { curr.depth + 1, curr.dir + 'U' };
+		}
+		else {
+			while (nextb.first - 1 >= 0 && board[nextb.first - 1][nextb.second] != '#') {
+				nextb.first--;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (canMove) {
+				while (nextr.first - 1 >= 0 && board[nextr.first - 1][nextr.second] != '#' &&
+					!(nextr.first - 1 == nextb.first && nextr.second == nextb.second)) {
+					nextr.first--;
+					if (board[nextr.first][nextr.second] == 'O') return { curr.depth + 1, curr.dir + 'U' };
+				}
+			}
+		}
+		if (canMove && !visited[nextr.first][nextr.second][nextb.first][nextb.second]) {
+			rq.push({ nextr.first, nextr.second, curr.depth + 1, curr.dir + 'U' });
+			bq.push(nextb);
+			visited[nextr.first][nextr.second][nextb.first][nextb.second] = true;
+		}
+
+		// го
+		nextr = { curr.y, curr.x };
+		nextb = curb;
+		canMove = true;
+		if (curr.y > curb.first) {
+			while (nextr.first + 1 < n && board[nextr.first + 1][nextr.second] != '#') {
+				nextr.first++;
+				if (board[nextr.first][nextr.second] == 'O') {
+					redGoal = true;
+					nextr = { 0,0 };
+					break;
+				}
+			}
+			while (nextb.first + 1 < n && board[nextb.first + 1][nextb.second] != '#' &&
+				!(nextr.first == nextb.first + 1 && nextr.second == nextb.second)) {
+				nextb.first++;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (redGoal && canMove) return { curr.depth + 1, curr.dir + 'D' };
+		}
+		else {
+			while (nextb.first + 1 < n && board[nextb.first + 1][nextb.second] != '#') {
+				nextb.first++;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (canMove) {
+				while (nextr.first + 1 < n && board[nextr.first + 1][nextr.second] != '#' &&
+					!(nextr.first + 1 == nextb.first && nextr.second == nextb.second)) {
+					nextr.first++;
+					if (board[nextr.first][nextr.second] == 'O') return { curr.depth + 1, curr.dir + 'D' };
+				}
+			}
+		}
+		if (canMove && !visited[nextr.first][nextr.second][nextb.first][nextb.second]) {
+			rq.push({ nextr.first, nextr.second, curr.depth + 1, curr.dir + 'D' });
+			bq.push(nextb);
+			visited[nextr.first][nextr.second][nextb.first][nextb.second] = true;
+		}
+
+		// аб
+		nextr = { curr.y, curr.x };
+		nextb = curb;
+		canMove = true;
+		if (curr.x < curb.second) {
+			while (nextr.second - 1 >= 0 && board[nextr.first][nextr.second - 1] != '#') {
+				nextr.second--;
+				if (board[nextr.first][nextr.second] == 'O') {
+					redGoal = true;
+					nextr = { 0,0 };
+					break;
+				}
+			}
+			while (nextb.second - 1 >= 0 && board[nextb.first][nextb.second - 1] != '#' &&
+				!(nextr.first == nextb.first && nextr.second == nextb.second - 1)) {
+				nextb.second--;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (redGoal && canMove) return { curr.depth + 1, curr.dir + 'L' };
+		}
+		else {
+			while (nextb.second - 1 >= 0 && board[nextb.first][nextb.second - 1] != '#') {
+				nextb.second--;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (canMove) {
+				while (nextr.second - 1 >= 0 && board[nextr.first][nextr.second - 1] != '#' &&
+					!(nextr.first == nextb.first && nextr.second - 1 == nextb.second)) {
+					nextr.second--;
+					if (board[nextr.first][nextr.second] == 'O') return { curr.depth + 1, curr.dir + 'L' };
+				}
+			}
+		}
+		if (canMove && !visited[nextr.first][nextr.second][nextb.first][nextb.second]) {
+			rq.push({ nextr.first, nextr.second, curr.depth + 1, curr.dir + 'L' });
+			bq.push(nextb);
+			visited[nextr.first][nextr.second][nextb.first][nextb.second] = true;
+		}
+
+		// ©Л
+		nextr = { curr.y, curr.x };
+		nextb = curb;
+		canMove = true;
+		if (curr.x > curb.second) {
+			while (nextr.second + 1 < m && board[nextr.first][nextr.second + 1] != '#') {
+				nextr.second++;
+				if (board[nextr.first][nextr.second] == 'O') {
+					redGoal = true;
+					nextr = { 0,0 };
+					break;
+				}
+			}
+			while (nextb.second + 1 < m && board[nextb.first][nextb.second + 1] != '#' &&
+				!(nextr.first == nextb.first && nextr.second == nextb.second + 1)) {
+				nextb.second++;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (redGoal && canMove) return { curr.depth + 1, curr.dir + 'R' };
+		}
+		else {
+			while (nextb.second + 1 < m && board[nextb.first][nextb.second + 1] != '#') {
+				nextb.second++;
+				if (board[nextb.first][nextb.second] == 'O') { canMove = false; break; }
+			}
+			if (canMove) {
+				while (nextr.second + 1 < m && board[nextr.first][nextr.second + 1] != '#' &&
+					!(nextr.first == nextb.first && nextr.second + 1 == nextb.second)) {
+					nextr.second++;
+					if (board[nextr.first][nextr.second] == 'O') return { curr.depth + 1, curr.dir + 'R' };
+				}
+			}
+		}
+		if (canMove && !visited[nextr.first][nextr.second][nextb.first][nextb.second]) {
+			rq.push({ nextr.first, nextr.second, curr.depth + 1, curr.dir + 'R' });
+			bq.push(nextb);
+			visited[nextr.first][nextr.second][nextb.first][nextb.second] = true;
+		}
+
+		if (curr.depth >= 10) return { -1,"" };
+	} return { -1,"" };
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	char input;
+	point hole, red, blue;
+	cin >> n >> m;
+	vector<vector<char>> board(n, vector<char>(m));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> input;
+			board[i][j] = input;
+			if (input == 'O') hole = { i,j };
+			else if (input == 'R') red = { i,j };
+			else if (input == 'B') blue = { i,j };
+		}
+	}
+	pair<int, string> ans = bfs(hole, red, blue, board);
+	if (ans.first == -1) cout << -1;
+	else cout << ans.first << '\n' << ans.second;
+
+	return 0;
+}
